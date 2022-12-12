2801c7ca747375281a3f47e32a5d93e1afe974fe
Amanda Christie Tarigan <mandachristie04@gmail.com>
2022-12-12 17:19:13 +0700

    fix feedback 2

--- lib/page/feedback_form.dart
+++ lib/page/feedback_form.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:bin_bank_app/utility/drawer_user.dart';
 import 'package:bin_bank_app/utility/drawer_public.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ import 'package:bin_bank_app/main.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:bin_bank_app/page/login_page.dart';
+import 'package:http/http.dart' as http;
 
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
@@ -32,9 +34,19 @@ class _MyFormPageState extends State<MyFormPage> {
     //final response = await http.post("http://127.0.0.1:8000/post-feedback-json/",
     //headers: {"Content-Type": "application/json"}, body: data);
 
-    final response = await request.postJson(
-        "http://127.0.0.1:8000/post-feedback-json/", data);
-    if (response['message'] == 'SUCCESS') {
+    var url = Uri.parse('http://127.0.0.1:8000/post-feedback-json/');
+    Map<String, String> requestBody = <String, String>{
+      "user": request.jsonData['username'],
+      "subject": subject,
+      "feedback": feedback,
+    };
+    var request1 = http.MultipartRequest('POST', url)
+      ..fields.addAll(requestBody);
+    var response = await request1.send();
+    final respStr = await response.stream.bytesToString();
+    var encoded = json.decode(respStr);
+
+    if (encoded['message'] == 'SUCCESS') {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
         content: Text("Feedback berhasil tersimpan"),
       ));
