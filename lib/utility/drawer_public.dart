54bdfcc38166f97ee3d2488269423a6372bb7119
syifaafra <afra.mumt@gmail.com>
2022-12-12 23:24:46 +0700

    add icon to drawer, update auth

--- lib/utility/drawer_public.dart
+++ lib/utility/drawer_public.dart
@@ -3,8 +3,6 @@ import 'package:bin_bank_app/page/register_page.dart';
 import 'package:bin_bank_app/page/leaderboard.dart';
 import 'package:flutter/material.dart';
 import 'package:bin_bank_app/main.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:provider/provider.dart';
 
 class MyDrawerPublic extends StatelessWidget {
   const MyDrawerPublic({Key? key}) : super(key: key);
@@ -26,6 +24,7 @@ class MyDrawerPublic extends StatelessWidget {
           runSpacing: 12,
           children: [
             ListTile(
+              leading: const Icon(Icons.home),
               title: const Text('HomePage'),
               onTap: () {
                 Navigator.of(context).pushReplacement(MaterialPageRoute(
@@ -35,6 +34,7 @@ class MyDrawerPublic extends StatelessWidget {
               },
             ),
             ListTile(
+              leading: const Icon(Icons.leaderboard_rounded),
               title: const Text('Leaderboard'),
               onTap: () {
                 Navigator.of(context).pushReplacement(MaterialPageRoute(
@@ -42,6 +42,7 @@ class MyDrawerPublic extends StatelessWidget {
               },
             ),
             ListTile(
+              leading: const Icon(Icons.login),
               title: const Text('Login'),
               onTap: () {
                 Navigator.of(context).pushReplacement(
@@ -49,6 +50,7 @@ class MyDrawerPublic extends StatelessWidget {
               },
             ),
             ListTile(
+              leading: const Icon(Icons.account_circle),
               title: const Text('Register'),
               onTap: () {
                 Navigator.of(context).pushReplacement(MaterialPageRoute(
--- lib/utility/drawer_user.dart
+++ lib/utility/drawer_user.dart
@@ -1,3 +1,4 @@
+import 'package:bin_bank_app/app_styles.dart';
 import 'package:bin_bank_app/page/deposit_sampah.dart';
 import 'package:bin_bank_app/page/leaderboard.dart';
 import 'package:bin_bank_app/page/history.dart';
@@ -7,7 +8,9 @@ import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
 class MyDrawerUser extends StatelessWidget {
+
   const MyDrawerUser({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) => Drawer(
@@ -20,49 +23,73 @@ class MyDrawerUser extends StatelessWidget {
         ),
       );
 
-  Widget buildMenuItems(BuildContext context) => Container(
-      padding: const EdgeInsets.all(24),
-      child: Wrap(
-        runSpacing: 12,
-        children: [
-          ListTile(
-            title: const Text('HomePage'),
-            onTap: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => const MyHomePage(
-                        title: 'Bin Bank',
-                      )));
-            },
-          ),
-          ListTile(
-            title: const Text('Leaderboard'),
-            onTap: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => const LeaderboardPage()));
-            },
-          ),
-          ListTile(
-            title: const Text('Donate'),
-            onTap: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => const MyDepositPage()));
-            },
-          ),
-          ListTile(
-            title: const Text('History'),
-            onTap: () {
-              Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => const HistoryPage()));
-            },
-          ),
-          ListTile(
-            title: const Text('Logout'),
-            // onTap: () {
-            //   logout(request);
-            //   Navigator.of(context).pushReplacement(MaterialPageRoute(
-            //       builder: (context) => const MyHomePage()));
-            // },
-          ),
-        ],
-      ));
+  Widget buildMenuItems(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    String username = request.jsonData['username'];
+    return Container(
+        padding: const EdgeInsets.all(24),
+        child: Wrap(
+          runSpacing: 12,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('HomePage'),
+              onTap: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const MyHomePage(
+                          title: 'Bin Bank',
+                        )));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.leaderboard_rounded),
+              title: const Text('Leaderboard'),
+              onTap: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const LeaderboardPage()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.card_giftcard),
+              title: const Text('Donate'),
+              onTap: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const MyDepositPage()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.timeline),
+              title: const Text('History'),
+              onTap: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const HistoryPage()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () async {
+                var response = await request.logout('https://bin-bank-pbp.up.railway.app/logout/ajax');
+                if (response['status'] == true) {
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("Berhasil Logout! Sampai jumpa kembali $username"),
+                    backgroundColor: kBlue,
+                  ));
+                  // ignore: use_build_context_synchronously
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) =>
+                          const MyHomePage(title: 'Bin Bank')));
+                } else {
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("Gagal Logout. Silakan coba lagi"),
+                    backgroundColor: kBlue,
+                  ));
+                }
+              },
+            ),
+          ],
+        ));
+  }
 }
